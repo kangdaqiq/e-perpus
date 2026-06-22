45fa6670df1cb685b57042cafa2edf35bcba7d08
kangdaqiq <kangdaqiq@gmail.com>
2026-06-22 21:57:24 +0700

    fix: tambah TrustProxies untuk support Cloudflare Tunnel dan reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Percayai semua proxy (Cloudflare Tunnel, Nginx, dll.)
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO |
+                     Request::HEADER_X_FORWARDED_PREFIX
+        );
+
+        // Kecualikan endpoint RFID dari verifikasi CSRF
         $middleware->validateCsrfTokens(except: [
             'api/rfid',
         ]);
@@ -18,3 +30,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
